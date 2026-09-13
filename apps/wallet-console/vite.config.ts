47d5276e963f41a9267190416fcf68677780a8ee
peitalin <n6378056@gmail.com>
2026-09-13 11:35:48 +0900

    fix: dedupe React in wallet frontend

--- apps/wallet-console/vite.config.ts
+++ apps/wallet-console/vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(() => {
       strictPort: true,
     },
     resolve: {
+      dedupe: ['react', 'react-dom'],
       alias: {
         '@': appSrc,
         '@core': `${appSrc}/core`,
